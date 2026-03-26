2d588ee00a6a7b7a87cd65392959e47095157b0e
NightsWatch Dev <dev@nightswatch.com>
2026-03-26 22:45:43 +0530

    add media module

---
+++ media-service/src/main/java/com/nightswatch/mediaservice/MediaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nightswatch.mediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaServiceApplication.class, args);
+    }
+}
